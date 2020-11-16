c81a7325d53424a14a0cdd684c549d04665beacd
Minobu Takahashi <ubuntu@ip-172-31-39-184.ap-northeast-1.compute.internal>
2020-11-16 14:35:28 +0000

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  # root 'application#hello'
+  
   get 'static_pages/home'
   # => StaticPages#home
   get 'static_pages/help'
   # => StaticPages#help
-  root 'application#hello'
+  
+  get 'static_pages/about'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
